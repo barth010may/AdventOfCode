e6df320dc05935fe491ecc3f2be222d38ca9f416
barth010may <borup.rasmus@gmail.com>
2022-12-08 18:15:59 +0100

    day 2 part 1 - completed

--- AdventofCode/Day 1/Program.cs
+++ AdventofCode/Day 1/Program.cs
@@ -1,18 +1,21 @@
 ﻿
 // Advent Calendar Day 1:
-// Find the Elf carrying the most calories
+
 
 using System.Diagnostics.CodeAnalysis;
 
 internal class Program {
     private static void Main(string[] args) {
 
-        string text = File.ReadAllText(@"C:\Users\Borup\C#\AdventOfCode\AdventofCode\Day 1\puzzleinput.txt");
+        string text = File.ReadAllText(@"C:\Users\Borup\AdventOfCode\AdventofCode\Day 1\puzzleinput.txt");
 
-        int sum = 0, biggestSum = 0,  answer = 0, s = 0;
+        int sum = 0, biggestSum = 0, answer = 0, s = 0, biggerSum = 0, bigSum =0;
         bool parsed;
 
         string[] subs = text.Split("\r\n");
+
+        // FIRST PART SOLUTION
+        // Find the sum of Elf carrying the most calories
 
         foreach (string sub in subs) {
 
@@ -32,6 +35,69 @@ internal class Program {
             }
         }
 
-        Console.WriteLine("Answer is: " + biggestSum);
+        Console.WriteLine("Answer to part 1: " + biggestSum);
+
+        // SECOND PART SOLUTION
+        // Find the sum of the top 3 elves carrying the most calories
+
+        // Check for the 2nd biggest
+
+        foreach (string sub in subs) {
+
+            parsed = int.TryParse(sub, out s);
+            if (parsed) {
+                s = int.Parse(sub);
+            }
+
+            if (sub == "") {
+                if (sum > biggerSum && sum < biggestSum) {
+                    biggerSum = sum;
+                }
+                sum = 0;
+            }
+            else {
+                sum += s;
+            }
+        }
+
+        
+        
+        // Check for the 3rd biggest
+        
+        foreach (string sub in subs) {
+
+            parsed = int.TryParse(sub, out s);
+            if (parsed) {
+                s = int.Parse(sub);
+            }
+
+            if (sub == "") {
+                if (sum > bigSum && sum < biggerSum) {
+                    bigSum = sum;
+                }
+                sum = 0;
+            }
+            else {
+                sum += s;
+            }
+        }
+
+        Console.WriteLine("Answer to Part 2: " + (biggestSum + biggerSum + bigSum));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
---
+++ AdventofCode/Day 2/Program.cs
@@ -0,0 +1,64 @@
+﻿using System.Reflection.PortableExecutable;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Day_2 {
+    public enum points {
+        A = 1,
+        B = 2,
+        C = 3,
+        X = 1,
+        Y = 2,
+        Z = 3,
+    };
+    internal class Program {
+        static void Main(string[] args) {
+
+            // Day 2, Part 1
+            // Final the total score of the given strategy guide
+
+            string text = File.ReadAllText(@"C:\Users\Borup\AdventOfCode\AdventofCode\Day 2\puzzleinput.txt");
+
+            string[] subs = text.Split("\r\n");
+
+            int sum = 0;
+
+            foreach (string sub in subs) {
+                switch (sub) {
+                    case "A X":
+                        sum += (int)points.X + 3;
+                        break;
+                    case "A Y":
+                        sum += (int)points.Y + 6;
+                        break;
+                    case "A Z":
+                        sum += (int)points.Z;
+                        break;
+                    case "B X":
+                        sum += (int)points.X;
+                        break;
+                    case "B Y":
+                        sum += (int)points.Y + 3;
+                        break;
+                    case "B Z":
+                        sum += (int)points.Z + 6;
+                        break;
+                    case "C X":
+                        sum += (int)points.X + 6;
+                        break;
+                    case "C Y":
+                        sum += (int)points.Y;
+                        break;
+                    case "C Z":
+                        sum += (int)points.Z + 3;
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            Console.WriteLine("Answer is: " + sum);
+
+
+        }
+    }
+}
