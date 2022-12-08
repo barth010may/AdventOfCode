3e757fe8a59f551940b50052ae4402c3a8beb635
barth010may <borup.rasmus@gmail.com>
2022-12-08 18:36:05 +0100

    completed Day 2, Part 2

--- AdventofCode/Day 2/Program.cs
+++ AdventofCode/Day 2/Program.cs
@@ -6,9 +6,9 @@ namespace Day_2 {
         A = 1,
         B = 2,
         C = 3,
-        X = 1,
-        Y = 2,
-        Z = 3,
+        Rock = 1,
+        Paper = 2,
+        Scissor = 3,
     };
     internal class Program {
         static void Main(string[] args) {
@@ -25,31 +25,31 @@ namespace Day_2 {
             foreach (string sub in subs) {
                 switch (sub) {
                     case "A X":
-                        sum += (int)points.X + 3;
+                        sum += (int)points.Rock + 3;
                         break;
                     case "A Y":
-                        sum += (int)points.Y + 6;
+                        sum += (int)points.Paper + 6;
                         break;
                     case "A Z":
-                        sum += (int)points.Z;
+                        sum += (int)points.Scissor;
                         break;
                     case "B X":
-                        sum += (int)points.X;
+                        sum += (int)points.Rock;
                         break;
                     case "B Y":
-                        sum += (int)points.Y + 3;
+                        sum += (int)points.Paper + 3;
                         break;
                     case "B Z":
-                        sum += (int)points.Z + 6;
+                        sum += (int)points.Scissor + 6;
                         break;
                     case "C X":
-                        sum += (int)points.X + 6;
+                        sum += (int)points.Rock + 6;
                         break;
                     case "C Y":
-                        sum += (int)points.Y;
+                        sum += (int)points.Paper;
                         break;
                     case "C Z":
-                        sum += (int)points.Z + 3;
+                        sum += (int)points.Scissor + 3;
                         break;
                     default:
                         break;
@@ -57,6 +57,48 @@ namespace Day_2 {
             }
 
             Console.WriteLine("Answer is: " + sum);
+
+            // DAY 2, PART 2
+            // Find the total score of the given strategy guide, but with a different scoring system
+
+            sum = 0;
+
+            foreach (string sub in subs) {
+                switch (sub) {
+                    case "A X":
+                        sum += (int)points.Scissor;
+                        break;
+                    case "A Y":
+                        sum += (int)points.Rock + 3;
+                        break;
+                    case "A Z":
+                        sum += (int)points.Paper + 6;
+                        break;
+                    case "B X":
+                        sum += (int)points.Rock;
+                        break;
+                    case "B Y":
+                        sum += (int)points.Paper + 3;
+                        break;
+                    case "B Z":
+                        sum += (int)points.Scissor + 6;
+                        break;
+                    case "C X":
+                        sum += (int)points.Paper;
+                        break;
+                    case "C Y":
+                        sum += (int)points.Scissor + 3;
+                        break;
+                    case "C Z":
+                        sum += (int)points.Rock + 6;
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            Console.WriteLine("Answer to Part 2: " + sum);
+
 
 
         }
