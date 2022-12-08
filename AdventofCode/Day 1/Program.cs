5f7fc00d6fe6aa44da505fc04b7ca866e9920eb0
barth010may <borup.rasmus@gmail.com>
2022-12-08 15:33:21 +0100

    Day 1 - TASK 1 completed

---
+++ AdventofCode/Day 1/Program.cs
@@ -0,0 +1,37 @@
+﻿
+// Advent Calendar Day 1:
+// Find the Elf carrying the most calories
+
+using System.Diagnostics.CodeAnalysis;
+
+internal class Program {
+    private static void Main(string[] args) {
+
+        string text = File.ReadAllText(@"C:\Users\Borup\C#\AdventOfCode\AdventofCode\Day 1\puzzleinput.txt");
+
+        int sum = 0, biggestSum = 0,  answer = 0, s = 0;
+        bool parsed;
+
+        string[] subs = text.Split("\r\n");
+
+        foreach (string sub in subs) {
+
+            parsed = int.TryParse(sub, out s);
+            if (parsed) {
+                s = int.Parse(sub);
+            }
+
+            if (sub == "") {
+                if (sum > biggestSum) {
+                    biggestSum = sum;
+                }
+                sum = 0;
+            }
+            else {
+                sum += s;
+            }
+        }
+
+        Console.WriteLine("Answer is: " + biggestSum);
+    }
+}
