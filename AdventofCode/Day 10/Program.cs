64d38b5ac3445e4b5e1f708b250f7d345503d757
barth010may <borup.rasmus@gmail.com>
2023-03-20 20:03:58 +0100

    day 10 and 9

---
+++ AdventofCode/Day 10/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Day_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // DAY 10, PART 1
+            // Find the sum of the signal strengths at certain cycles
+
+
+        }
+    }
+}
---
+++ AdventofCode/Day 9/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Day_9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // DAY 9, PART 1
+            // Find the amount of squares the tail visited atleast once
+
+            string text = File.ReadAllText(@"C:\Users\Borup\C#\AdventOfCode\AdventofCode\Day 9\puzzleinput.txt");
+
+            string[] lines = text.Split("\r\n");
+
+            foreach (string line in lines) {
+                Console.WriteLine(line);
+            }
+
+
+
+
+                
+        }
+            
+    }
+}
