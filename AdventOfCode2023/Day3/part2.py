c1f6a60710e66aabc67467020b7283fe7a940755
barth010may <borup.rasmus@gmail.com>
2023-12-03 11:33:39 +0100

    Finished Day 3

---
+++ AdventOfCode2023/Day3/part2.py
@@ -0,0 +1,86 @@
+# DAY 3: https://adventofcode.com/2023/day/3
+import re
+
+puzzleInput = open('input.txt').read().split('\n')
+
+rows, cols = (len(puzzleInput), len(puzzleInput[0]))
+
+
+sum = 0
+# loops through each row of the input
+for j in range(rows):
+    # For each row it finds the symbols and saves them in a list of Match Objects
+    symbols = re.finditer("[\*]", puzzleInput[j])
+
+    # Check for horizontal adjacent
+    for symbol in symbols:
+        span = symbol.span()
+        counter = 0
+        gearRatio = 1
+        # Check for left horizontal
+        left = puzzleInput[j][span[0] - 1]
+        if left.isdigit():
+            left_instance = re.search("[0-9]+", puzzleInput[j][span[0] - 3:span[0]])
+            gearRatio *= int(left_instance.group())
+            counter += 1
+
+        # Check for right horizontal
+        right = puzzleInput[j][span[0] + 1]
+        if right.isdigit():
+            right_instance = re.search("[0-9]+", puzzleInput[j][span[0]:span[1] + 3])
+            gearRatio *= int(right_instance.group())
+            counter += 1
+
+        # Check for up vertical
+        up = puzzleInput[j-1][span[0]]
+        up_left = puzzleInput[j-1][span[0] - 1]
+        up_right = puzzleInput[j-1][span[0] + 1]
+        if up.isdigit():
+            up_instance_right = re.search("[0-9]+", puzzleInput[j-1][span[0]:span[1] + 3])
+            up_instance_left = re.findall("[0-9]+", puzzleInput[j-1][span[0] - 3:span[1]])
+            up_instance_left.reverse()
+            up_instance_middle = re.findall("[0-9]+", puzzleInput[j-1][span[0] - 1:span[1]+1])
+            number = int(up_instance_right.group()) if int(up_instance_right.group()) > int(up_instance_left[0]) else int(up_instance_left[0])
+            number = number if number > int(up_instance_middle[0]) else int(up_instance_middle[0])
+            gearRatio *= number
+            counter += 1
+        # Check for up diagonal left
+        elif up_left.isdigit() or up_right.isdigit():
+            if up_left.isdigit():
+                left_instance = re.search("[0-9]+", puzzleInput[j-1][span[0] - 3:span[0]])
+                gearRatio *= int(left_instance.group())
+                counter += 1
+            if up_right.isdigit():
+                right_instance = re.search("[0-9]+", puzzleInput[j-1][span[0]:span[1] + 3])
+                gearRatio *= int(right_instance.group())
+                counter += 1
+
+        # Check for down vertical
+        down = puzzleInput[j + 1][span[0]]
+        down_left = puzzleInput[j + 1][span[0] - 1]
+        down_right = puzzleInput[j + 1][span[0] + 1]
+        if down.isdigit():
+            down_instance_right = re.search("[0-9]+", puzzleInput[j+1][span[0]:span[1] + 3])
+            down_instance_left = re.findall("[0-9]+", puzzleInput[j+1][span[0] - 3:span[1]])
+            down_instance_left.reverse()
+            down_instance_middle = re.findall("[0-9]+", puzzleInput[j+1][span[0] - 1:span[1]+1])
+            number = int(down_instance_right.group()) if int(down_instance_right.group()) > int(down_instance_left[0]) else int(down_instance_left[0])
+            number = number if number > int(down_instance_middle[0]) else int(down_instance_middle[0])
+            gearRatio *= number
+            counter += 1
+            # Check for up diagonal left
+        elif down_left.isdigit() or down_right.isdigit():
+            if down_left.isdigit():
+                left_instance = re.search("[0-9]+", puzzleInput[j + 1][span[0] - 3:span[0]])
+                gearRatio *= int(left_instance.group())
+                counter += 1
+            if down_right.isdigit():
+                right_instance = re.search("[0-9]+", puzzleInput[j + 1][span[0]:span[1] + 3])
+                gearRatio *= int(right_instance.group())
+                counter += 1
+        if counter == 2:
+            sum += gearRatio
+
+
+print(f"Total Sum = {sum}")
+
